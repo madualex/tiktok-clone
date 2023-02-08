516f228aacd45576ecb7f2023a836c2b3d2ebd65
alexmadu <ogochukwumadu.ng@gmail.com>
2023-02-08 05:17:04 +0100

    ...

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  
   reactStrictMode: true,
   images: {
     domains: ['file-uploads.teachablecdn.com', "lh3.googleusercontent.com"]
